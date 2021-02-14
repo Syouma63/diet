64fd69f3c1f667d5d72d1a71528fdfec9ec5995e
Shouma63 <choma63.twins24@gmail.com>
2021-02-14 17:23:31 +0900

    chart

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/mypage', (req, res) => {
   res.render('mypage.ejs');
 });
 
+app.get('/chart', (req,res) => {
+  res.render('chart.ejs');
+});
+
 
 
 app.listen(3000);
--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -3,16 +3,17 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // 作成するグラフの種類
-    type: 'line',
+    type: 'bar',
 
     // ラベルとデータセットを設定
     data: {
-      labels: ['8/1', '8/2', '8/3', '8/4', '8/5', '8/6', '8/7', '8/8', '8/9', '8/10', '8/11', '8/12', '8/13', '8/14','8/15', '8/16', '8/17', '8/18', '8/19', '8/20', '8/21','8/22', '8/23', '8/24', '8/25', '8/26', '8/27', '8/28', '8/29', '8/30', '8/31'],
+      labels: ['8/1', '8/8', '8/9', '8/13', '8/16', '8/19'],
         datasets: [{
-            label: "体重（kg）",
-            // backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [68.7, 67.9, 68.5, 67.6, 67, 67.4, 66.8, 68.7, 67.9, 67.5, 67.6, 67, 67.4, 66.8, 68.7, 67.9, 68.5, 67.6, 67, 67.4, 66.8, 68.7, 67.9, 68.5, 67.6, 67, 67.4, 66.8, 67, 67.4, 66,8],
+          label: "スコア",
+          // backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [150, 130, 100, 98, 120, 110],
+          backgroundColor: "rgba(219,39,91,0.5)"
         }]
     },
 
@@ -25,11 +26,12 @@ var chart = new Chart(ctx, {
           {
             // 目盛り
             ticks: {
-              min: 50,
-              msuggestedMax: 80,
+              msuggestedMin: 50,
+              msuggestedMax: 120,
               stepSize: 5,
               callback: function(value, index, values){
-                return  value +  'kg'
+                // return  value +  'kg'
+                return value
               }
             }
           }
