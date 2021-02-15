ebd250e482c180769b50c5adf4361a3d3d308f59
Shouma63 <choma63.twins24@gmail.com>
2021-02-15 10:27:23 +0900

    login-signup

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express');
-
-
+const mysql = require('mysql')
+const session = require('express-session')
 const app = express();
 
-
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'pila'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
+app.use(
+  session({
+    secret: 'my_secret_key',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
 
 
 app.get('/', (req, res) => {
@@ -12,6 +37,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/mypage', (req, res) => {
+  if (req.session.userId === undefined) {
+    console.log('ログインしていません');
+  } else {
+    console.log('ログインしています');
+  }
   res.render('mypage.ejs');
 });
 
@@ -22,6 +52,51 @@ app.get('/chart', (req,res) => {
 app.get('/record', (req,res) => {
   res.render('record.ejs');
 });
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs');
+});
+
+app.post('/signup', (req, res) => {
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+
+  connection.query(
+    'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+    [username, email, password],
+    (error, results) => {
+      res.redirect('/mypage');
+    }
+  );
+})
+
+app.get('/login', (req, res) => {
+  connection.query(
+    res.render('login.ejs')
+  )
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  connection.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
+    (error, results) => {
+      if (results.length > 0) {
+        if (req.body.password === results[0].password) {
+          req.session.userId = results[0].id;
+          res.redirect('/mypage');
+        } else {
+          res.redirect('/login');
+        }
+      } else {
+        res.redirect('/login');
+      }
+    }
+  )
+});
+
 
 
 
